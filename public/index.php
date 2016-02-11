79e2d23be4a175355832431be7c76bd75225fa8c
Derek Nutile <dereknutile@gmail.com>
2016-02-11 12:40:05 -0800

    Adds logic in the main index that reads the full xml file and outputs the groups as a test.

--- public/index.php
+++ public/index.php
@@ -1,57 +1,75 @@
 <html>
-  <head>
-    <title>VLEMP</title>
+    <head>
+        <title>XML Parser</title>
 
-    <link href='//fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
-    <link href='//fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+        <link href='//fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
+        <link href='//fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
 
-    <style>
-      body {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-        color: #aaa;
-        display: table;
-        font-weight: 100;
-        font-family: 'Roboto';
-        text-align: center;
-      }
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                color: #aaa;
+                font-weight: 100;
+                font-family: 'Roboto';
+            }
 
-      .container {
-        text-align: center;
-        display: table-cell;
-        vertical-align: middle;
-      }
+            .container {
+                margin-left: auto;
+                margin-right: auto;
+                width: 75%;
+            }
 
-      .content {
-        width: 640px;
-        display: inline-block;
-      }
+            .title {
+                padding-top: 1em;
+                font-family: 'Raleway';
+                font-size: 3em;
+                color: #001;
+                margin-bottom: 1em;
+            }
 
-      .title {
-        font-family: 'Raleway';
-        font-size: 48px;
-        color: #001;
-        margin-bottom: 40px;
-      }
+            p {
+                font-size: 1.3em;
+                line-height: 2em;
+                color: #011;
+                text-align: left;
+            }
+        </style>
+    </head>
 
-      p {
-        font-size: 20px;
-        line-height: 30px;
-        color: #011;
-        text-align: left;
-      }
-    </style>
-  </head>
+    <body>
+        <div class="container">
+            <div class="title"><?php echo "Title"; ?></div>
 
-  <body>
-    <div class="container">
-      <div class="content">
-        <div class="title"><?php echo "Your Vagrant LEMP installation is good to go!"; ?></div>
-        <p>This index.php file is located in the root of this installation under <code>/public/</code>.  You can delete this index.php file and start developing your code in the public directory now!</p>
-        <p>The <a href="https://github.com/dereknutile/vlemp" target="_blank">VLEMP Github repository</a> has more information about the default server configuration, installed software, and tools.</p>
-      </div><!-- /.content -->
-    </div><!-- /.container -->
-  </body>
+            <?php
+
+            // Set a variable for the source data directory
+            $data_directory = __DIR__.'/../data';
+
+            // Load the target xml file into an XML object using simplexml_load_file
+            $xml = simplexml_load_file($data_directory.'/import.xml' );
+
+            // Iterate
+            foreach ($xml->GroupMap->Group as $group) {
+                putp("The group named <b>".$group['name']."</b> has an ID of <b>".$group['id']."</b>.");
+            }
+
+            // Dump a variable with a little HTML dressing for readability
+            function dd($string){
+                echo '<pre>';
+                var_dump($string);
+                echo '</pre>';
+                echo '<hr />';
+            }
+
+            // Dump a simple string into a paragraph HTML wrapper
+            function putp($string){
+                echo "<p>";
+                echo $string;
+                echo "</p>";
+            }
+            ?>
+
+        </div><!-- /.container -->
+    </body>
 </html>
