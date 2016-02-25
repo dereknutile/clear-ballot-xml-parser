3419884a98510191a25e94db5535f56e56ee3cbc
Derek Nutile <dereknutile@gmail.com>
2016-02-25 11:05:20 -0800

    Creates a sample config file to base the config.php file on.

---
+++ app/config/example.config.php
@@ -0,0 +1,13 @@
+<?php
+    // Application Settings
+    $app_title = "TITLE OF APP";
+
+    // Set core directories
+    $data_directory = __DIR__.'/../data/';
+    $app_directory = __DIR__.'/../app/';
+    $partials_directory = __DIR__.'/../partials/';
+    $config_directory = __DIR__.'/../config/';
+
+    // Grab common functions
+    require($config.'functions.php');
+?>
