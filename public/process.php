a3fd5924686950c70e411e91a607105baa99d122
Derek Nutile <dereknutile@gmail.com>
2016-03-15 14:13:26 -0700

    Copies the old index.php, which was acting as the parse engine, to a file named process.php.

---
+++ public/process.php
@@ -0,0 +1,37 @@
+<?php
+    /**
+     * Bootstrap the application
+     */
+    require(__DIR__.'/../app/bootstrap.php');
+
+    /**
+     * Instantiate the class
+     */
+    $e = new Washco\Election;
+
+    /**
+     * Set class variables
+     */
+    $e->importFile = $data_directory.$import_file;
+    $e->outputFile = $output_directory.$output_file;
+    $e->partialsDirectory = $partials_directory;
+    $e->appTitle = $app_title;
+    $e->logoUrl = $logo_url;
+    if(isset($next)){
+        $e->nextTime = $next;
+    }
+
+    /**
+     * Build the html string
+     */
+    $e->addPartial('head.html');
+    $e->addPartial('nav.html');
+    $e->processImportFile();
+    $e->addPartial('foot.html');
+
+    /**
+     * Write the completed html string
+     */
+    $e->writeOutputFile(false);
+    $e->dd("Parse Complete.")
+?>
