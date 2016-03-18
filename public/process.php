b5f2d6c1e387b2a2f2783d8bac2cb78b8b003ffc
Derek Nutile <dereknutile@gmail.com>
2016-03-17 17:25:47 -0700

    Starts to add logic for handling post variables in the processor.

--- public/process.php
+++ public/process.php
@@ -10,42 +10,45 @@
      */
     $e = new Washco\Election;
 
-    /**
-     * Set class variables
-     */
-    $e->importFile = $data_directory.$import_file;
-    $e->outputFile = $output_directory.$output_file;
-    $e->partialsDirectory = $partials_directory;
-    $e->appTitle = $app_title;
-    $e->logoUrl = $logo_url;
-    if(isset($next)){
-        $e->nextTime = $next;
+    // array(4) { ["status"]=> string(4) "none" ["date"]=> string(0) "" ["time"]=> string(4) "none" ["file"]=> string(23) "import-washco-empty.xml" }
+
+    $output = "No input file";
+
+    if($_POST['file']){
+
+        $file = html_entity_decode(trim($_POST['file']));
+        /**
+         * Set class variables
+         */
+        $e->importFile = $data_directory.$file;
+        $e->outputFile = $output_directory.$output_file;
+        $e->partialsDirectory = $partials_directory;
+        $e->appTitle = $app_title;
+        $e->logoUrl = $logo_url;
+        if(isset($next)){
+            $e->nextTime = $next;
+        }
+
+        /**
+         * Build the html string
+         */
+        $e->addPartial('head.html');
+        $e->addPartial('nav.html');
+        $e->processImportFile();
+        $e->addPartial('foot.html');
+
+        /**
+         * Write the completed html string
+         */
+        $e->writeOutputFile(false);
+        $output = $file." processed";
     }
-
-    /**
-     * Build the html string
-     */
-    $e->addPartial('head.html');
-    $e->addPartial('nav.html');
-    $e->processImportFile();
-    $e->addPartial('foot.html');
-
-    /**
-     * Write the completed html string
-     */
-    $e->writeOutputFile(false);
 
     include($partials_directory.'/head.html');
     include($partials_directory.'/nav.html');
 
     echo '<div class="container add-top-padding">';
-    var_dump($_POST);
-    if($_POST['file']){
-        echo "<p>File: ".htmlspecialchars($_POST['file'])."</p>";
-    } else {
-        echo "<p>No file posted.</p>";
-    }
-
+    echo '<h2>'.$output.'</h2>';
     echo '</div>';
 
     include($partials_directory.'/foot.html');
