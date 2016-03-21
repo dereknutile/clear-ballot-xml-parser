94a61da5c434d4aa57d68e67bd84407659097e32
Derek Nutile <dereknutile@gmail.com>
2016-03-21 11:25:36 -0700

    Fixes paths in the config example.

--- app/config/example.config.php
+++ app/config/example.config.php
@@ -2,9 +2,11 @@
     // Set core directories
     $app_directory = __DIR__.'/../app/';
     $config_directory = __DIR__.'/../config/';
-    $data_directory = __DIR__.'/../../data/';
-    $output_directory = __DIR__.'/../../public/';
+    $data_directory = __DIR__.'/../../input/';
+    $output_directory = __DIR__.'/../../output/';
+    $processed_directory = __DIR__.'/../../processed/';
     $partials_directory = __DIR__.'/../partials/';
+    $public_directory = __DIR__.'/../../public/';
 
     // Set file names
     $import_file = 'import.xml';
